1477e2c42e805cbb80fe9563926ebbc406b1870f
klabarre <klabarre@dawan.fr>
2024-12-02 16:08:40 +0100

    ajout Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import JsxTheorie from "./view/JsxTheorie";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<JsxTheorie />} />
+          <Route path="/products" element={<>Page product</>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
