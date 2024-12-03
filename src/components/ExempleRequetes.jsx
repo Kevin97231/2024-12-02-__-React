4e98ed45b85f35885d2f89b503809dead3451e92
klabarre <klabarre@dawan.fr>
2024-12-03 15:02:46 +0100

    ajout correction

--- src/components/ExempleRequetes.jsx
+++ src/components/ExempleRequetes.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 
 export const ExempleRequetes = () => {
   const url = "http://localhost:3001/products";
 
   const [products, setProducts] = useState([]);
+
+  const formRef = useRef(null);
 
   useEffect(() => {
     // const fetchProduct = () => {
@@ -36,7 +38,28 @@ export const ExempleRequetes = () => {
     fetchProduct2();
   }, []);
 
-  const addProduct = () => {};
+  const addProduct = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    console.log(formData);
+
+    const newProduct = {
+      category: formData.get("category"),
+      name: formData.get("name"),
+      price: formData.get("price"),
+      number: formData.get("number"),
+    };
+
+    axios
+      .post(url, newProduct)
+      .then((response) => setProducts((prev) => [...prev, response.data]))
+      .finally(() => {
+        // formRef.current.reset();
+        myForm.reset();
+      });
+  };
 
   return (
     <>
@@ -67,12 +90,32 @@ export const ExempleRequetes = () => {
       </section>
       {/* Section formulaire */}
       <section>
-        <form onSubmit={addProduct}>
+        <form onSubmit={addProduct} ref={formRef} id="myForm">
           <div className="grid grid-cols-4 mb-5 gap-5">
-            <input />
-            <input />
-            <input />
-            <input />
+            <input
+              type="text"
+              placeholder="Nom du produit"
+              name="name"
+              className="flex"
+            />
+            <input
+              type="number"
+              placeholder="Prix"
+              name="price"
+              className="flex"
+            />
+            <input
+              type="number"
+              placeholder="Quantité"
+              name="number"
+              className="flex"
+            />
+            <input
+              type="text"
+              placeholder="Catégorie"
+              name="category"
+              className="flex"
+            />
           </div>
           <button type="submit" className="btn">
             Ajouter
